f5aa4db75958a814c95bf118add29cc6da202866
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-12-16 17:15:55 -0800

    show me the money

--- client/controllers/ShowController.js
+++ client/controllers/ShowController.js
@@ -1,6 +1,28 @@
-app.controller('showController', ['friendsFactory','$location', function(friendsFactory, $location) {
+app.controller('showController', ['friendsFactory','$location','$routeParams', function(friendsFactory, $location, $routeParams) {
    var _this = this;
-   _this.friend = {}
+   _this.friend = {};
+
+   // _this.getFriend = function(){
+   //    friendsFactory.getFriend(function gotFriend(friend){
+   //       console.log('i got a friend: ',friend);
+   //    });
+   //
+   // }
+
+
+
+   _this.getFriend = function() {
+      friendsFactory.show($routeParams.id, function passedToFriendFactoryShow(friend) {
+      //console.log('this is the friend, based on the id: ',friend);
+      _this.friend = friend;
+    })
+  }
+
+
+
+   _this.getFriend();
+
+
 
    // _this.newFriendCreatedNowRedirect = function(){
    //    //console.log('FRIEND created redirect called');
