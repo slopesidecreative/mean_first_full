a0a268ba6a96a748f5f94c41e79d652c9788ccaf
Erik Clineschmidt <eclineschmidt@gmail.com>
2016-12-16 16:45:45 -0800

    birthday search

--- client/controllers/IndexController.js
+++ client/controllers/IndexController.js
@@ -1,33 +1,29 @@
 app.controller('indexController', ['$scope','friendsFactory', '$location','moment', function($scope, friendsFactory, location, moment) {
 
-console.log(moment);
-  /* Private Methods */
-  var friendsIndex = function() {
-     console.log('index controller -> usersIndex() called to kick things off...');
-      friendsFactory.index(function beingPassedToTheFactoryIndexByThisController(friendsFromTheFactory) {
-        $scope.friends = friendsFromTheFactory;
-      } /* end args passed to userFactor index */ ); //end userFactory method invokation
-   } //end friendsIndex
+var index = function() {
+  console.log('index controller -> index() called to kick things off...');
+   friendsFactory.index(function beingPassedToTheFactoryIndexByThisController(friendsFromTheFactory) {
+     $scope.friends = friendsFromTheFactory;
+   });
+}
 
-
-$scope.deleteFriend = function(_id){
+$scope.delete = function(_id){
    friendsFactory.delete(_id, function redirectAfterDelete(data){
       console.log('successfully deleted: ',data );
       location.path('/');
-   })
+   });
 }
 
 $scope.show = function(_id){
-   location.path('/friends/' + id );
+   location.path('/friends/' + _id );
 }
 
-  /* Scope Methods */
-  $scope.update = function(user_id) {
-      location.path('/friends/' + user_id + '/edit');
-    }
-    /* on load time */
-  console.log("loading the controller");
-  //console.log(userFactory);
-  //console.log(this);
-  friendsIndex();
+$scope.update = function(user_id) {
+   location.path('/friends/' + user_id + '/edit');
+ }
+
+ /* on load time */
+console.log("loading the controller");
+index();
+
 }]);
